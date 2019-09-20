8d667185189e84d91990b9ed8155bf0e56d2f6cd
Lyle Dickie <lyle.e.dickie@gmail.com>
2019-09-20 09:24:23 -0400

    created Song class and initialized.

--- lib/song.rb
+++ lib/song.rb
@@ -1,1 +1,53 @@
-puts "running"
+require "pry"
+
+class Song
+
+  attr_accessor :name, :artist, :genre
+
+  @@all = []
+
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@all << self
+
+
+
+  end
+
+
+  def count
+
+
+  end
+
+
+  def artists
+
+
+
+  end
+
+
+  def genres
+
+
+  end
+
+
+  def genre_count
+
+
+
+  end
+
+  def artist_count
+
+
+
+  end
+
+
+
+end
